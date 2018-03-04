63b5cdb92e4421288ca85ca12972d443905bcbbd
NadiaQN <quezada.nadia@gmail.com>
2018-03-04 15:36:53 -0300

    agregando funcionalidad

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('section').hide();
+  $('#portfolio').show();
+});
+
+$('#btn-portfolio').click(function() {
+  $('section').hide();
+  $('#portfolio').show();
+});
+$('#btn-about').click(function() {
+  $('section').hide();
+  $('#about-me').show();
+});
+$('#btn-tools').click(function() {
+  $('section').hide();
+  $('#tools').show();
+});
+$('#btn-contact').click(function() {
+  $('section').hide();
+  $('#contact').show();
+});
