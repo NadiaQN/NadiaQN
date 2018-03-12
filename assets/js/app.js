616d7decf6e458e966cb976026471d15ea3deb1d
NadiaQN <quezada.nadia@gmail.com>
2018-03-12 18:29:12 -0300

    mejorando el responsive y cambio de colores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-  $('section').hide();
-  $('#portfolio').show();
+  $('.button-collapse').sideNav({
+    closeOnClick: true
+  });
+  $('ul.tabs').tabs('select_tab', 'tab_id');
+
+  $('.btn-portfolio').click(function() {
+    $('.info').hide();
+    $('#portfolio').show();
+  });
+
+  $('.btn-about').click(function() {
+    $('.info').hide();
+    $('#about-me').show();
+  });
+
+  $('.btn-tools').click(function() {
+    $('.info').hide();
+    $('#tools').show();
+  });
+
+  $('.btn-contact').click(function() {
+    $('.info').hide();
+    $('#contact-me').show();
+  });
 });
 
-$('#btn-portfolio').click(function() {
-  $('section').hide();
-  $('#portfolio').show();
-});
-$('#btn-about').click(function() {
-  $('section').hide();
-  $('#about-me').show();
-});
-$('#btn-tools').click(function() {
-  $('section').hide();
-  $('#tools').show();
-});
-$('#btn-contact').click(function() {
-  $('section').hide();
-  $('#contact').show();
-});
